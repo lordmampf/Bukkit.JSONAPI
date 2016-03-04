6550e315af2395b5bdf8c56a5f4f87c9188994fb
lordmampf <lordmampf@users.noreply.github.com>
2016-03-04 03:22:05 +0100

    ProtocolLib as softdepend -unknow what isn't working without it

--- src/main/java/com/alecgorge/minecraft/jsonapi/packets/netty/JSONAPINettyInjector.java
+++ src/main/java/com/alecgorge/minecraft/jsonapi/packets/netty/JSONAPINettyInjector.java
@@ -2,25 +2,34 @@ package com.alecgorge.minecraft.jsonapi.packets.netty;
 
 import io.netty.channel.Channel;
 
+import org.bukkit.Bukkit;
+
 import com.alecgorge.minecraft.jsonapi.JSONAPI;
 import com.alecgorge.minecraft.jsonapi.packets.netty.router.JSONAPIDefaultRoutes;
 
 public class JSONAPINettyInjector {
-	NettyInjector injector = null;
-	
+	private NettyInjector injector = null;
+
 	public JSONAPINettyInjector(final JSONAPI api) {
+		if (Bukkit.getServer().getPluginManager().getPlugin("ProtocolLib") == null) {
+			Bukkit.getLogger().info("ProtocolLib not found - try to work without.");
+			return;
+		}
+
 		injector = new NettyInjector() {
-	        @Override
-	        protected void injectChannel(Channel channel) {
-	            channel.pipeline().addFirst(new JSONAPIChannelDecoder(api));
-	        }
-	    };
-	    injector.inject();
-	    
-	    new JSONAPIDefaultRoutes(api);
+			@Override
+			protected void injectChannel(Channel channel) {
+				channel.pipeline().addFirst(new JSONAPIChannelDecoder(api));
+			}
+		};
+		injector.inject();
+
+		new JSONAPIDefaultRoutes(api);
+
 	}
-	
+
 	public void close() {
-		injector.close();
+		if (injector != null)
+			injector.close();
 	}
 }
--- src/main/java/com/alecgorge/minecraft/jsonapi/packets/netty/NettyInjector.java
+++ src/main/java/com/alecgorge/minecraft/jsonapi/packets/netty/NettyInjector.java
@@ -17,137 +17,140 @@ import com.comphenix.protocol.reflect.VolatileField;
 import com.comphenix.protocol.utility.MinecraftReflection;
 import com.google.common.collect.Lists;
 
-public abstract class NettyInjector {   
-    // The temporary player factory
-    private List<VolatileField> bootstrapFields = Lists.newArrayList();
-   
-    // List of network managers
-    private volatile List<Object> networkManagers;
-    private boolean injected;
+public abstract class NettyInjector {
+	// The temporary player factory
+	private List<VolatileField> bootstrapFields = Lists.newArrayList();
 
-    private boolean closed;
+	// List of network managers
+	private volatile List<Object> networkManagers;
+	private boolean injected;
 
-    /**
-     * Inject into the spigot connection class.
-     */
-    @SuppressWarnings("unchecked")
-    public synchronized void inject() {
-        if (injected)
-            throw new IllegalStateException("Cannot inject twice.");
-        try {
-            FuzzyReflection fuzzyServer = FuzzyReflection.fromClass(MinecraftReflection.getMinecraftServerClass());
-            Method serverConnectionMethod = fuzzyServer.getMethodByParameters("getServerConnection", MinecraftReflection.getServerConnectionClass(), new Class[] {});
-            
-            // Get the server connection
-            Object server = fuzzyServer.getSingleton();
-            Object serverConnection = serverConnectionMethod.invoke(server);
-            
-            // Handle connected channels
-            final ChannelInboundHandler endInitProtocol = new ChannelInitializer<Channel>() {
-                @Override
-                protected void initChannel(Channel channel) throws Exception {
-                    try {
-                        // This can take a while, so we need to stop the main thread from interfering
-                        synchronized (networkManagers) {
-                            injectChannel(channel);
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
-            
-            // This is executed before Minecraft's channel handler
-            final ChannelInboundHandler beginInitProtocol = new ChannelInitializer<Channel>() {
-                @Override
-                protected void initChannel(Channel channel) throws Exception {
-                    // Our only job is to add init protocol
-                    channel.pipeline().addLast(endInitProtocol);
-                }
-            };
-            
-            // Add our handler to newly created channels
-            final ChannelHandler connectionHandler = new ChannelInboundHandlerAdapter() {
-                @Override
-                public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                    Channel channel = (Channel) msg;
+	private boolean closed;
 
-                    // Prepare to initialize ths channel
-                    channel.pipeline().addFirst(beginInitProtocol);
-                    ctx.fireChannelRead(msg);
-                }
-            };
-            
-            // Get the current NetworkMananger list
-            networkManagers = (List<Object>) FuzzyReflection.fromObject(serverConnection, true).
-                invokeMethod(null, "getNetworkManagers", List.class, serverConnection);
-            
-            // Insert ProtocolLib's connection interceptor
-            bootstrapFields = getBootstrapFields(serverConnection);
-            
-            for (VolatileField field : bootstrapFields) {
-                final List<Object> list = (List<Object>) field.getValue();
-     
-                // We don't have to override this list
-                if (list == networkManagers) {
-                    continue;
-                }
-                
-                // Synchronize with each list before we attempt to replace them.
-                field.setValue(new BootstrapList(list, connectionHandler));
-            }
+	/**
+	 * Inject into the spigot connection class.
+	 */
+	@SuppressWarnings("unchecked")
+	public synchronized void inject() {
+		if (injected)
+			throw new IllegalStateException("Cannot inject twice.");
+		try {
+			FuzzyReflection fuzzyServer = FuzzyReflection.fromClass(MinecraftReflection.getMinecraftServerClass());
+			Method serverConnectionMethod = fuzzyServer.getMethodByParameters("getServerConnection", MinecraftReflection.getServerConnectionClass(),
+					new Class[] {});
 
-            injected = true;
-            
-        } catch (Exception e) {
-            throw new RuntimeException("Unable to inject channel futures.", e);
-        }
-    }
-    
-    /**
-     * Invoked when a channel is ready to be injected.
-     * @param channel - the channel to inject.
-     */
-    protected abstract void injectChannel(Channel channel);
-    
-    /**
-     * Retrieve a list of every field with a list of channel futures.
-     * @param serverConnection - the connection.
-     * @return List of fields.
-     */
-    private List<VolatileField> getBootstrapFields(Object serverConnection) {
-        List<VolatileField> result = Lists.newArrayList();
-        
-        // Find and (possibly) proxy every list
-        for (Field field : FuzzyReflection.fromObject(serverConnection, true).getFieldListByType(List.class)) {
-            VolatileField volatileField = new VolatileField(field, serverConnection, true).toSynchronized();
-            
-            @SuppressWarnings("unchecked")
-            List<Object> list = (List<Object>) volatileField.getValue();
-            
-            if (list.size() == 0 || list.get(0) instanceof ChannelFuture) {
-                result.add(volatileField);
-            }
-        }
-        return result;
-    }
-    
-    /**
-     * Clean up any remaning injections.
-     */
-    public synchronized void close() {
-        if (!closed) {
-            closed = true;
+			// Get the server connection
+			Object server = fuzzyServer.getSingleton();
+			Object serverConnection = serverConnectionMethod.invoke(server);
 
-            for (VolatileField field : bootstrapFields) {
-                Object value = field.getValue();
+			// Handle connected channels
+			final ChannelInboundHandler endInitProtocol = new ChannelInitializer<Channel>() {
+				@Override
+				protected void initChannel(Channel channel) throws Exception {
+					try {
+						// This can take a while, so we need to stop the main thread from interfering
+						synchronized (networkManagers) {
+							injectChannel(channel);
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			};
 
-                // Undo the processed channels, if any 
-                if (value instanceof BootstrapList) {
-                    ((BootstrapList) value).close();
-                }
-                field.revertValue();
-            }
-        }
-    }
+			// This is executed before Minecraft's channel handler
+			final ChannelInboundHandler beginInitProtocol = new ChannelInitializer<Channel>() {
+				@Override
+				protected void initChannel(Channel channel) throws Exception {
+					// Our only job is to add init protocol
+					channel.pipeline().addLast(endInitProtocol);
+				}
+			};
+
+			// Add our handler to newly created channels
+			final ChannelHandler connectionHandler = new ChannelInboundHandlerAdapter() {
+				@Override
+				public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+					Channel channel = (Channel) msg;
+
+					// Prepare to initialize ths channel
+					channel.pipeline().addFirst(beginInitProtocol);
+					ctx.fireChannelRead(msg);
+				}
+			};
+
+			// Get the current NetworkMananger list
+			networkManagers = (List<Object>) FuzzyReflection.fromObject(serverConnection, true).invokeMethod(null, "getNetworkManagers", List.class,
+					serverConnection);
+
+			// Insert ProtocolLib's connection interceptor
+			bootstrapFields = getBootstrapFields(serverConnection);
+
+			for (VolatileField field : bootstrapFields) {
+				final List<Object> list = (List<Object>) field.getValue();
+
+				// We don't have to override this list
+				if (list == networkManagers) {
+					continue;
+				}
+
+				// Synchronize with each list before we attempt to replace them.
+				field.setValue(new BootstrapList(list, connectionHandler));
+			}
+
+			injected = true;
+
+		} catch (Exception e) {
+			throw new RuntimeException("Unable to inject channel futures.", e);
+		}
+	}
+
+	/**
+	 * Invoked when a channel is ready to be injected.
+	 * 
+	 * @param channel - the channel to inject.
+	 */
+	protected abstract void injectChannel(Channel channel);
+
+	/**
+	 * Retrieve a list of every field with a list of channel futures.
+	 * 
+	 * @param serverConnection - the connection.
+	 * @return List of fields.
+	 */
+	private List<VolatileField> getBootstrapFields(Object serverConnection) {
+		List<VolatileField> result = Lists.newArrayList();
+
+		// Find and (possibly) proxy every list
+		for (Field field : FuzzyReflection.fromObject(serverConnection, true).getFieldListByType(List.class)) {
+			VolatileField volatileField = new VolatileField(field, serverConnection, true).toSynchronized();
+
+			@SuppressWarnings("unchecked")
+			List<Object> list = (List<Object>) volatileField.getValue();
+
+			if (list.size() == 0 || list.get(0) instanceof ChannelFuture) {
+				result.add(volatileField);
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Clean up any remaning injections.
+	 */
+	public synchronized void close() {
+		if (!closed) {
+			closed = true;
+
+			for (VolatileField field : bootstrapFields) {
+				Object value = field.getValue();
+
+				// Undo the processed channels, if any 
+				if (value instanceof BootstrapList) {
+					((BootstrapList) value).close();
+				}
+				field.revertValue();
+			}
+		}
+	}
 }
